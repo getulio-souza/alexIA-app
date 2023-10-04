1c4aeb1c00e6b1a83e6a31764517744897a7e2fb
getulio.souza <getulio.souza@evolutix.com.br>
2023-10-03 22:19:44 -0300

    feature:links-to-other-pages

--- src/components/home-page/home.js
+++ src/components/home-page/home.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 import {
   HomeNavbar,
   KnowMore,
@@ -12,26 +14,50 @@ import {
   HomeHeaderDescription,
   HomeHeaderImg,
   HomeBenefits,
-  GenericBorder,
+  // GenericBorder,
   SingleBenefit,
   SingleBenefitText,
   SingleBenefitTitle,
   SingleBenefitDescription,
+  // SingleBenefitImgBox,
   SingleBenefitImg,
   HomeQuestions,
+  HomeQuestionsTitle,
+  HomeQuestionsContainer,
+  SingleHomeQuestion,
+  SingleHomeQuestionTitle,
+  SingleHomeQuestionText,
   HomeNewsletter,
+  HomeNewsletterTitle,
+  HomeNewsletterInfo,
+  HomeNewsletterEmailInput,
+  HomeNewsletterSubscribeBtn,
+  HomeNewsletterSocialMedia,
+  InstagramIcon,
+  FacebookIcon,
+  HomeFooter,
+  HomeFooterIcon,
+  HomeFooterCopyright,
 } from "./home.styles";
 
-import alexia from '../../assets/ia-second-example.jpg'
-import pacientAndDoctor from '../../assets/patient-and-doctor.jpg'
+import alexia from "../../assets/ia-second-example.jpg";
+import pacientAndDoctor from "../../assets/patient-and-doctor.jpg";
+import faceIcon from "../../assets/facebook.png";
+import InstaIcon from "../../assets/instagram.png";
 
 function home() {
   return (
     <>
       <HomeNavbar>
-        <KnowMore>Saiba mais</KnowMore>
-        <Plans>Planos</Plans>
-        <Contact>Contato</Contact>
+        <Link style={{textDecoration:'none'}} to="/knowMore">
+          <KnowMore>Saiba mais</KnowMore>
+        </Link>
+        <Link style={{textDecoration:'none'}} to="/plans">
+          <Plans>Planos</Plans>
+        </Link>
+        <Link style={{textDecoration:'none'}} to="/contact">
+          <Contact>Contato</Contact>
+        </Link>
       </HomeNavbar>
       <HomeHeader>
         <HomeHeaderText>
@@ -50,18 +76,100 @@ function home() {
       </HomeHeader>
       <PageUnderline></PageUnderline>
       <HomeBenefits>
-        <GenericBorder>
-          <SingleBenefit>
-            <SingleBenefitText>
-            <SingleBenefitTitle>Chat Privado,Dinâmico e Eficaz</SingleBenefitTitle>
-            <SingleBenefitDescription>O paciente interage diretamente com a AlexIA por meio de um chat privado para realizar sua triagem com rapidez e segurança.</SingleBenefitDescription>
-            </SingleBenefitText>
-            <SingleBenefitImg src={pacientAndDoctor}></SingleBenefitImg>
-          </SingleBenefit>
-        </GenericBorder>
+        <SingleBenefit>
+          <SingleBenefitText>
+            <SingleBenefitTitle>
+              Chat Privado,Dinâmico e Eficaz
+            </SingleBenefitTitle>
+            <SingleBenefitDescription>
+              O paciente interage diretamente com a AlexIA por meio de um chat
+              privado para realizar sua triagem com rapidez e segurança.
+            </SingleBenefitDescription>
+          </SingleBenefitText>
+          <SingleBenefitImg src={pacientAndDoctor}></SingleBenefitImg>
+        </SingleBenefit>
+        <SingleBenefit>
+          <SingleBenefitText>
+            <SingleBenefitTitle>
+              Padrões e Sintomas Identificados pelo FHIR
+            </SingleBenefitTitle>
+            <SingleBenefitDescription>
+              AlexIA usa o padrão FHIR para encontrar padrões e correlações
+              entre sintomas, indicando possíveis causas com maior
+              assertividade.
+            </SingleBenefitDescription>
+          </SingleBenefitText>
+          <SingleBenefitImg src={pacientAndDoctor}></SingleBenefitImg>
+        </SingleBenefit>
+        <SingleBenefit>
+          <SingleBenefitText>
+            <SingleBenefitTitle>Triagem Inovadora</SingleBenefitTitle>
+            <SingleBenefitDescription>
+              AlexIA revoluciona a triagem de pacientes com uso da inteligência
+              artificial, agilizando o processo e garantindo diagnósticos mais
+              precisos e eficientes.
+            </SingleBenefitDescription>
+          </SingleBenefitText>
+          <SingleBenefitImg src={pacientAndDoctor}></SingleBenefitImg>
+        </SingleBenefit>
       </HomeBenefits>
-      <HomeQuestions></HomeQuestions>
-      <HomeNewsletter></HomeNewsletter>
+      <PageUnderline></PageUnderline>
+      <HomeQuestions>
+        <HomeQuestionsTitle>Perguntas Frequentes</HomeQuestionsTitle>
+        <HomeQuestionsContainer>
+          <SingleHomeQuestion>
+            <SingleHomeQuestionTitle>
+              A AlexIA é segura?
+            </SingleHomeQuestionTitle>
+            <SingleHomeQuestionText>
+              Sim! A segurança dos seus dados é nossa prioridade. Utilizamos
+              criptografia e práticas avançadas de segurança para proteger suas
+              informações.
+            </SingleHomeQuestionText>
+          </SingleHomeQuestion>
+          <SingleHomeQuestion>
+            <SingleHomeQuestionTitle>
+              A AlexIA substitui médicos?
+            </SingleHomeQuestionTitle>
+            <SingleHomeQuestionText>
+              A AlexIA não pretende substituir médicos, mas sim complementar o
+              atendimento tradicional, oferecendo diagnósticos mais rápidos e
+              agilizando seus cuidados médicos.
+            </SingleHomeQuestionText>
+          </SingleHomeQuestion>
+          <SingleHomeQuestion>
+            <SingleHomeQuestionTitle>
+              Como a AlexIA funciona?
+            </SingleHomeQuestionTitle>
+            <SingleHomeQuestionText>
+              A AlexandrIA utiliza uma base de dados FHIR para compreender os
+              problemas dos pacientes e gerar diagnósticos médicos precisos.
+            </SingleHomeQuestionText>
+          </SingleHomeQuestion>
+        </HomeQuestionsContainer>
+      </HomeQuestions>
+      <PageUnderline></PageUnderline>
+      <HomeNewsletter>
+        <HomeNewsletterTitle>
+          Receba atualizações sobre AlexIA
+        </HomeNewsletterTitle>
+        <HomeNewsletterInfo>
+          Se inscreva em nossa newsletter e receba as noticias mais recentes
+          sobre AlexIA
+        </HomeNewsletterInfo>
+        <HomeNewsletterEmailInput></HomeNewsletterEmailInput>
+        <HomeNewsletterSubscribeBtn>Inscrever-se</HomeNewsletterSubscribeBtn>
+        <HomeNewsletterSocialMedia>
+          <InstagramIcon src={InstaIcon}></InstagramIcon>
+          <FacebookIcon src={faceIcon}></FacebookIcon>
+        </HomeNewsletterSocialMedia>
+      </HomeNewsletter>
+      <HomeFooter>
+        <HomeFooterIcon></HomeFooterIcon>
+        <HomeFooterCopyright>
+          Direitos autorais AlexIA Plataforma de Saúde
+        </HomeFooterCopyright>
+      </HomeFooter>
     </>
   );
 }
--- src/components/home-page/home.styles.js
+++ src/components/home-page/home.styles.js
@@ -91,10 +91,108 @@ export const SingleBenefitTitle = styled.h3``;
 
 export const SingleBenefitDescription = styled.p``;
 
+export const SingleBenefitImgBox = styled.div``
+
 export const SingleBenefitImg = styled.img`
   width: 30%;
   height: 30%;
 `;
 
-export const HomeQuestions = styled.div``;
-export const HomeNewsletter = styled.div``;
+export const HomeQuestions = styled.div`
+  padding:40px;
+`;
+export const HomeQuestionsTitle = styled.h2`
+  text-align:center;
+  padding-bottom:40px;
+`;
+export const HomeQuestionsContainer = styled.div`
+  display:flex;
+  justify-content:space-evenly;
+  gap:30px;
+`;
+
+export const SingleHomeQuestion = styled.div`
+  width:25%;
+`;
+export const SingleHomeQuestionTitle = styled.h3``;
+export const SingleHomeQuestionText = styled.p``;
+
+export const HomeNewsletter = styled.div`
+  padding:40px;
+  text-align:center;
+  display:flex;
+  flex-direction:column;
+`;
+
+export const HomeNewsletterTitle = styled.h2`
+  width:28%;
+  margin: 0 auto;
+  padding-bottom:20px;
+`;
+
+export const HomeNewsletterInfo = styled.p`
+  font-size:12px;
+  width:30%;
+  margin: 0 auto;
+  padding-bottom:20px;
+`;
+
+export const HomeNewsletterEmailInput = styled.input`
+  width:413px;
+  height:41px;
+  border-radius:10px;
+  border: 1px solid #8E8484;
+  margin-bottom: 20px;
+  margin: 0 auto;
+  font-size: 20px;
+  padding-left:2%;
+  text-align:center;
+`;
+
+export const HomeNewsletterSubscribeBtn = styled.button`
+  border:none;
+  background-color:#1F1F1F;
+  color:#fff;
+  width: 20%;
+  height:40px;
+  margin: 0 auto;
+  margin-top:20px;
+  border-radius: 20px;
+  cursor:pointer;
+  transition: 0.5s;;
+  &:hover{
+    opacity:0.9;
+  }
+`;
+
+export const HomeNewsletterSocialMedia = styled.div`
+  display:flex;
+  gap:40px;
+  justify-content:center;
+  padding-top:40px
+`;
+
+export const InstagramIcon = styled.img`
+  width: 20px;
+  cursor:pointer;
+  transition: 0.5s;;
+  &:hover{
+    opacity:0.8;
+  }
+`;
+
+export const FacebookIcon = styled.img`
+  width: 20px;
+  cursor:pointer;
+  transition: 0.5s;;
+  &:hover{
+    opacity:0.8;
+  }
+`;
+
+export const HomeFooter = styled.div`
+  text-align:center;
+`;
+
+export const HomeFooterIcon = styled.div``;
+export const HomeFooterCopyright = styled.div``;
