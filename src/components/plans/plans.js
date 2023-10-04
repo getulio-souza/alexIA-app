86eaffdaa1fe376cdf99aee6b4ba09c9515d729c
getulio.souza <getulio.souza@evolutix.com.br>
2023-10-04 20:35:10 -0300

    feature: plans-done

--- src/components/plans/plans.js
+++ src/components/plans/plans.js
@@ -1,9 +1,92 @@
-import React from 'react'
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+import {
+  HomeNavbar,
+  KnowMore,
+  OurPlans,
+  Contact,
+  PlansTitle,
+  PlansSubtitle,
+  SinglePlanContainer,
+  SinglePlan,
+  SinglePlanBox,
+  SinglePlanPriceBox,
+  SinglePlanTitle,
+  SinglePlanDescription,
+  SinglePlanBtn,
+  SinglePlanPrice,
+  HomeFooter,
+  HomeFooterIcon,
+  HomeFooterCopyright
+} from "./plans.styles";
+
+import lines from '../../assets/10.png'
+
 
 function Plans() {
   return (
-    <div>Plans</div>
-  )
+    <>
+      <HomeNavbar>
+        <Link style={{ textDecoration: "none" }} to="/knowMore">
+          <KnowMore>Saiba mais</KnowMore>
+        </Link>
+        <Link style={{ textDecoration: "none" }} to="/plans">
+          <OurPlans>Planos</OurPlans>
+        </Link>
+        <Link style={{ textDecoration: "none" }} to="/contact">
+          <Contact>Contato</Contact>
+        </Link>
+      </HomeNavbar>
+      <PlansTitle>Planos</PlansTitle>
+      <PlansSubtitle>
+        AlexIA tem diversos planos para atender seus clientes, escolha o mais
+        eficiente para você, e tenha acesso imediato
+      </PlansSubtitle>
+      <SinglePlanContainer>
+        <SinglePlan>
+          <SinglePlanBox>
+            <SinglePlanTitle>Plano Basic</SinglePlanTitle>
+            <SinglePlanDescription>O plano Basic te dá as funcionalidades fundamentais de AlexIA, é possível fazer triagens gerais, com um tempo maior de espera, é ideal para quem quer testar o produto sem investir tanto.</SinglePlanDescription>
+            <SinglePlanBtn>Saiba mais</SinglePlanBtn>
+          </SinglePlanBox>
+          <SinglePlanPriceBox>
+            <SinglePlanPrice>R$</SinglePlanPrice>
+            <SinglePlanPrice>299.90</SinglePlanPrice>
+          </SinglePlanPriceBox>
+        </SinglePlan>
+        <SinglePlan>
+          <SinglePlanBox>
+            <SinglePlanTitle>Plano Platinum</SinglePlanTitle>
+            <SinglePlanDescription>O plano starter te dá todas as funcionalidades da AlexIA, porém, com um tempo maior de espera e uma quantidade de usuários simultâneos limitados.</SinglePlanDescription>
+            <SinglePlanBtn>Saiba mais</SinglePlanBtn>
+          </SinglePlanBox>
+          <SinglePlanPriceBox>
+          <SinglePlanPrice>R$</SinglePlanPrice>
+          <SinglePlanPrice>599.90</SinglePlanPrice>
+          </SinglePlanPriceBox>
+        </SinglePlan>
+        <SinglePlan>
+          <SinglePlanBox>
+            <SinglePlanTitle>Plano Diamond</SinglePlanTitle>
+            <SinglePlanDescription>O plano starter te dá todas as funcionalidades da AlexIA, porém, com um tempo maior de espera e uma quantidade de usuários simultâneos limitados.</SinglePlanDescription>
+            <SinglePlanBtn>Saiba mais</SinglePlanBtn>
+          </SinglePlanBox>
+          <SinglePlanPriceBox>
+            <SinglePlanPrice>R$</SinglePlanPrice>
+            <SinglePlanPrice>999.90</SinglePlanPrice>
+          </SinglePlanPriceBox>
+        </SinglePlan>
+      </SinglePlanContainer>
+      <HomeFooter>
+        <HomeFooterIcon src={lines}></HomeFooterIcon>
+        <HomeFooterCopyright>
+          Direitos autorais AlexIA Plataforma de Saúde
+        </HomeFooterCopyright>
+      </HomeFooter>
+    </>
+  );
 }
 
-export default Plans
+export default Plans;
--- src/components/plans/plans.styles.js
+++ src/components/plans/plans.styles.js
@@ -1,1 +1,121 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
+
+export const HomeNavbar = styled.div`
+  background-color: #1E1E1E;
+  width: 639px;
+  height: 61px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin: 0 auto;
+  margin-top: 20px;
+  border-radius:15px;
+`;
+
+export const KnowMore = styled.h3`
+  font-size:24px;
+  color: #fff;
+  cursor: pointer;
+  transition: .5s;
+  &:hover{
+    opacity:0.8;
+  }
+`;
+
+export const OurPlans = styled.h3`
+  font-size:24px;
+  color: #fff;
+  cursor: pointer;
+  transition: .5s;
+  &:hover{
+    opacity:0.8;
+  }
+`;
+
+export const Contact = styled.h3`
+  font-size:24px;
+  color: #fff;
+  cursor: pointer;
+  transition: .5s;
+  &:hover{
+    opacity:0.8;
+  }
+`;
+
+//plans structure
+
+export const PlansTitle = styled.h2`
+  text-align:center;
+  padding-top:40px;
+`;
+
+export const PlansSubtitle = styled.h6`
+  text-align:center;
+  min-width:30%;
+  width:30%;
+  max-width:30%;
+  margin:0 auto;
+`;
+
+export const SinglePlanContainer = styled.div`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  flex-direction: column;
+  padding:40px 0px;
+`;
+
+export const SinglePlan = styled.div`
+  width: 50%;
+  margin: 0 auto;
+  display: flex;
+  align-items:center;
+  border: 1px solid #000;
+  margin: 20px;
+  padding: 20px;
+`;
+
+export const SinglePlanBox = styled.div`
+  padding: 20px;
+`;
+
+export const SinglePlanTitle = styled.h3``;
+export const SinglePlanDescription = styled.p`
+  width:60%;
+`;
+export const SinglePlanBtn = styled.button`
+  border: none;
+  background-color: #000;
+  color: #fff;
+  width: 150px;
+  height: 30px;
+  border-radius: 10px;
+  transition: .5s;
+  cursor: pointer;
+  &:hover{
+    opacity:0.8;
+  }
+`;
+
+export const SinglePlanPriceBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items:center;
+`
+
+export const SinglePlanPrice = styled.div`
+  font-weight:900;
+  font-size:30px
+`;
+
+export const HomeFooter = styled.div`
+  text-align:center;
+`;
+
+export const HomeFooterIcon = styled.img`
+  width:90px;
+  color:#000;
+`;
+export const HomeFooterCopyright = styled.p`
+  font-size:12px;
+`;
