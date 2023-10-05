ec6c102e76f4e0f471d0bc399e68292e914832d5
getulio.souza <getulio.souza@evolutix.com.br>
2023-10-04 22:02:55 -0300

    feature: basic-plan

--- src/components/plans/basic/basicPlan.js
+++ src/components/plans/basic/basicPlan.js
@@ -1,9 +1,84 @@
-import React from 'react'
+import React from "react";
+
+import {
+  PlansTitle,
+  SinglePlan,
+  SinglePlanBox,
+  SinglePlanPriceBox,
+  SinglePlanTitle,
+  SinglePlanDescription,
+  SinglePlanPrice,
+  PlanUserInfo,
+  PlanUserEmail,
+  PlanUserLabel,
+  PlanUserInput,
+  PlanUserCNPJ,
+  PlanUserPayment,
+  PlanUserCreditCardTitle,
+  PlanUserCreditCardSelect,
+  PlanUserPaymentPortion,
+  PlanUserPortionOption,
+  PlanConfirmBtn
+} from "./basicPlan.styles";
+
+
+import Navbar from "../../navbar/navbar";
 
 function BasicPlan() {
   return (
-    <div>basicPlan</div>
-  )
+    <>
+      <Navbar/>
+      <PlansTitle>Assinatura</PlansTitle>
+      <SinglePlan>
+        <SinglePlanBox>
+          <SinglePlanTitle>Plano Basic</SinglePlanTitle>
+          <SinglePlanDescription>
+            O plano Basic te dá as funcionalidades fundamentais de AlexIA, é
+            possível fazer triagens gerais, com um tempo maior de espera, é
+            ideal para quem quer testar o produto sem investir tanto.
+          </SinglePlanDescription>
+        </SinglePlanBox>
+        <SinglePlanPriceBox>
+          <SinglePlanPrice>R$</SinglePlanPrice>
+          <SinglePlanPrice>299.90</SinglePlanPrice>
+        </SinglePlanPriceBox>
+      </SinglePlan>
+      <PlanUserInfo>
+        <PlanUserEmail>
+          <PlanUserLabel>E-mail</PlanUserLabel>
+          <PlanUserInput
+            type="email"
+          ></PlanUserInput>
+        </PlanUserEmail>
+        <PlanUserCNPJ>
+        <PlanUserLabel>CNPJ</PlanUserLabel>
+          <PlanUserInput
+            maxLength={14}
+          ></PlanUserInput>
+        </PlanUserCNPJ>
+        <PlanUserPayment>
+          <PlanUserCreditCardTitle>Forma de pagamento</PlanUserCreditCardTitle>
+          <PlanUserCreditCardSelect>
+            <option>Cartão de Crédito - Mastercard</option>
+            <option>Cartão de Crédito - Visa</option>
+            <option>Cartão de Crédito - Electron</option>
+          </PlanUserCreditCardSelect>
+        </PlanUserPayment>
+        <PlanUserPaymentPortion>
+          <PlanUserPortionOption
+            placeholder="1x de R$ 299,90"
+          >
+            <option>1x de R$ 299,90</option>
+            <option>2x de R$ 199,90</option>
+            <option>3x de R$ 99,90</option>
+            <option>4x de R$ 59,90</option>
+            <option>5x de R$ 29,90</option>
+          </PlanUserPortionOption>
+        </PlanUserPaymentPortion>
+      <PlanConfirmBtn>Finalizar compra</PlanConfirmBtn>
+      </PlanUserInfo>
+    </>
+  );
 }
 
-export default BasicPlan
+export default BasicPlan;
--- src/components/plans/basic/basicPlan.styles.js
+++ src/components/plans/basic/basicPlan.styles.js
@@ -1,1 +1,151 @@
 import styled from "styled-components";
+
+export const PlansTitle = styled.h2`
+  text-align:center;
+  padding-top:40px;
+`;
+
+export const PlansSubtitle = styled.h6`
+  text-align:center;
+  min-width:30%;
+  width:30%;
+  max-width:30%;
+  margin:0 auto;
+`;
+
+export const SinglePlanContainer = styled.div`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  flex-direction: column;
+  padding:40px 0px;
+`;
+
+export const SinglePlan = styled.div`
+  width: 50%;
+  margin: 0 auto;
+  display: flex;
+  align-items:center;
+  border: 1px solid #000;
+  padding: 20px;
+`;
+
+export const SinglePlanBox = styled.div`
+  padding: 20px;
+`;
+
+export const SinglePlanTitle = styled.h3``;
+export const SinglePlanDescription = styled.p`
+  width:60%;
+`;
+export const SinglePlanBtn = styled.button`
+  border: none;
+  background-color: #000;
+  color: #fff;
+  width: 150px;
+  height: 30px;
+  border-radius: 10px;
+  transition: .5s;
+  cursor: pointer;
+  &:hover{
+    opacity:0.8;
+  }
+`;
+
+export const SinglePlanPriceBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items:center;
+`
+
+export const SinglePlanPrice = styled.div`
+  font-weight:900;
+  font-size:30px
+`;
+
+export const PlanUserInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 40px 0px;
+`;
+export const PlanUserEmail = styled.div`
+  display: flex;
+  flex-direction:column;
+  align-items:flex-start;
+  padding:10px 0px;
+  width: 30%;
+`;
+
+export const PlanUserLabel = styled.label`
+  padding-bottom:5px;
+`;
+
+export const PlanUserInput = styled.input`
+  width:120%;
+  height:30px;
+  border-radius:5px;
+  border:1px solid grey;
+  padding-left:10px;
+`;
+
+export const PlanUserCNPJ = styled.div`
+  display: flex;
+  flex-direction:column;
+  align-items:flex-start;
+  padding:10px 0px;
+  width: 30%;
+`;
+
+export const PlanUserPayment = styled.div`
+  display: flex;
+  flex-direction:column;
+  align-items:flex-start;
+  padding:10px 0px;
+  width:30%;
+`;
+
+export const PlanUserCreditCardTitle = styled.label`
+  padding-bottom:5px;
+`;
+
+export const PlanUserCreditCardSelect = styled.select`
+  min-width:120%;
+  height:30px;
+  border-radius:5px;
+  background-color: transparent;
+  border:1px solid grey;
+  cursor: pointer;
+`;
+
+export const PlanUserPaymentPortion = styled.div`
+  display: flex;
+  flex-direction:column;
+  align-items:flex-start;
+  padding:10px 0px;
+  width:30%;
+
+`;
+
+export const PlanUserPortionOption = styled.select`
+  min-width:120%;
+  height:30px;
+  border-radius:5px;
+  border:1px solid grey;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+export const PlanConfirmBtn = styled.button`
+  margin-top:20px;
+  width:150px;
+  height:40px;
+  background-color:#1F1F1F;
+  color:#fff;
+  border-radius:10px;
+  cursor: pointer;
+  transition: .5s;
+  &:hover{
+    opacity:0.8;
+  }
+`;
